267dff7261a55852e52749c67b703d79191510e4
gwcaccadm <gwc.acc.adm@gmail.com>
2022-09-21 14:58:21 +1000

    Make directory loop

--- PSModulesDeploy.cmd
+++ PSModulesDeploy.cmd
@@ -1,3 +1,12 @@
+:checkFolder
+
+if not exist "C:\ProgramData\AutoPilotConfig\PDFMerge" (
+    mkdir "C:\ProgramData\AutoPilotConfig\PDFMerge"
+    set /a CheckFolderResult = 1
+)
+
+if %*CheckFolderResult*% EQU 0 goto checkFolder
+
 xcopy "PSModules.ps1" "C:\ProgramData\AutoPilotConfig" /Y
 xcopy "pswritepdf.0.0.19.nupkg" "C:\ProgramData\AutoPilotConfig" /Y
 Powershell.exe -Executionpolicy bypass -File "C:\ProgramData\AutoPilotConfig\PSModules.ps1"
